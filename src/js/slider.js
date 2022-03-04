d7302488190a044799d5f2f48b246841a34976eb
Hanna <hanna@flitdesk.com>
2022-03-04 17:25:32 +0100

    refactor(slider): improve js code

--- src/js/slider.js
+++ src/js/slider.js
@@ -3,48 +3,30 @@
 const slider = document.querySelector(".slider");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
-const sliderButtonLeft = document.querySelector(".slider__button--left");
-const sliderButtonRight = document.querySelector(".slider__button--right");
 const sliderWidth = slider.scrollWidth - slider.clientWidth;
 let scrollAmount = 0;
 
 previous.addEventListener("click", () => {
 	if (scrollAmount > 30) {
-		sliderButtonRight.classList.remove("d-none");
+		next.classList.remove("d-none");
 		scrollAmount = slider.scrollLeft -= 30;
 	} else {
-		sliderButtonLeft.classList.add("d-none");
+		previous.classList.add("d-none");
 	}
-	console.log("scrollAmount = " + scrollAmount, "sliderWidth =" + sliderWidth);
 });
 
 next.addEventListener("click", () => {
 	if (scrollAmount < sliderWidth) {
-		sliderButtonLeft.classList.remove("d-none");
+		previous.classList.remove("d-none");
 		scrollAmount = slider.scrollLeft += 30;
 	} else {
-		sliderButtonRight.classList.add("d-none");
+		next.classList.add("d-none");
 	}
-	console.log("scrollAmount = " + scrollAmount, "sliderWidth =" + sliderWidth);
 });
 
 // carousel
 
 const carouselItem = document.querySelectorAll(".carousel__item");
-
-if (screen.width > 992) {
-	carouselItem.forEach((element) => {
-		element.classList.remove("carousel-item");
-	});
-} else {
-	carouselItem.forEach((element) => {
-		element.classList.add("carousel-item");
-	});
-}
-
-window.addEventListener("resize", () => {
-	screenWidth();
-});
 
 function screenWidth() {
 	if (screen.width > 992) {
@@ -57,3 +39,9 @@ function screenWidth() {
 		});
 	}
 }
+
+screenWidth();
+
+window.addEventListener("resize", () => {
+	screenWidth();
+});
